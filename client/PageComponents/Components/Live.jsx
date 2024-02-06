88d74eb515bab0265c5fbde05c984446976e34fb
Adarsh Mishra <adjee2020@gmail.com>
2024-02-06 10:08:36 +0530

    few changes

--- client/PageComponents/Components/Live.jsx
+++ client/PageComponents/Components/Live.jsx
@@ -6,8 +6,9 @@ import {
   faBitcoin,
   faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
+import Loader from "./Loader/Loader";
 
-const Live = ({ properties }) => {
+const Live = ({ properties, isLoading }) => {
   console.log(properties);
   const n = properties.length;
   return (
@@ -23,65 +24,72 @@ const Live = ({ properties }) => {
           </div>
         </div>
         <div className="row g-5">
-          {properties
-            .map((property, idx) => (
-              <div
-                key={idx + 1}
-                className="col-5 col-lg-4 col-md-6 col-sm-6 col-12"
-              >
-                <div className="product-style-one">
-                  <div className="card-thumbnail">
-                    <Link href={`/detail?property=${property.productID}`}>
-                      <img
-                        src={property.image}
-                        alt={property.title}
-                        style={{ height: "200px" }} // Set a fixed height
-                      />
-                    </Link>
-                  </div>
-                  <div className="product-share-wrapper">
-                    <div className="profile-share">
-                      {property.reviewers &&
-                        property.reviewers.length !== 0 &&
-                        property.reviewers.map((el, i) => (
-                          <Link
-                            href="#"
-                            className="avatar"
-                            data-tooltip={`${el.slice(0, 15)}...`}
-                          >
-                            <img
-                              src={`/client/client-${i + 1}.png`}
-                              alt="Nft_Profile"
-                            />
+          {isLoading ? (
+            <Loader />
+          ) : properties.length == 0 ? (
+            <h1>No Property</h1>
+          ) : (
+            properties
+              .map((property, idx) => (
+                <div
+                  key={idx + 1}
+                  className="col-5 col-lg-4 col-md-6 col-sm-6 col-12"
+                >
+                  <div className="product-style-one">
+                    <div className="card-thumbnail">
+                      <Link href={`/detail?property=${property.productID}`}>
+                        <img
+                          src={property.image}
+                          alt={property.title}
+                          style={{ height: "200px" }} // Set a fixed height
+                        />
+                      </Link>
+                    </div>
+                    <div className="product-share-wrapper">
+                      <div className="profile-share">
+                        {property.reviewers &&
+                          property.reviewers.length !== 0 &&
+                          property.reviewers.map((el, i) => (
+                            <Link
+                              href="#"
+                              className="avatar"
+                              data-tooltip={`${el.slice(0, 15)}...`}
+                            >
+                              <img
+                                src={`/client/client-${i + 1}.png`}
+                                alt="Nft_Profile"
+                              />
+                            </Link>
+                          ))}
+                        {property.reviewers &&
+                        property.reviewers.length !== 0 ? (
+                          <Link className="more-author-text" href="#">
+                            Interested User
                           </Link>
-                        ))}
-                      {property.reviewers && property.reviewers.length !== 0 ? (
-                        <Link className="more-author-text" href="#">
-                          Interested User
-                        </Link>
-                      ) : (
-                        <>New</>
-                      )}
+                        ) : (
+                          <>New</>
+                        )}
+                      </div>
+                    </div>
+                    <Link href={`/detail?property=${property.productID}`}>
+                      <span className="product-name">
+                        <FontAwesomeIcon icon={faHome} className="icon" />{" "}
+                        {property.title.length >= 25
+                          ? `${property.title.slice(0, 22)}...`
+                          : property.title}
+                      </span>
+                    </Link>
+                    <span className="latest-bid" style={{ marginTop: "100px" }}>
+                      {property.category}
+                    </span>
+                    <div className="bid-react-area">
+                      <div className="last-bid">{property.price} MATIC</div>
                     </div>
                   </div>
-                  <Link href={`/detail?property=${property.productID}`}>
-                    <span className="product-name">
-                      <FontAwesomeIcon icon={faHome} className="icon" />{" "}
-                      {property.title.length >= 25
-                        ? `${property.title.slice(0, 22)}...`
-                        : property.title}
-                    </span>
-                  </Link>
-                  <span className="latest-bid" style={{ marginTop: "100px" }}>
-                    {property.category}
-                  </span>
-                  <div className="bid-react-area">
-                    <div className="last-bid">{property.price} MATIC</div>
-                  </div>
                 </div>
-              </div>
-            ))
-            .slice(n - 6, n)}
+              ))
+              .slice(Math.max(0, n - 6), n)
+          )}
         </div>
         {/* Button to show all properties */}
         <div className="text-center mt-4">
